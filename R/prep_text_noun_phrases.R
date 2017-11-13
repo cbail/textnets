76d534382ecde391f1d8654ad0c0116e9ef46914
Taylor Brown <taylorwhittenbrown@gmail.com>
2017-11-13 06:26:33 -0500

    fix 'remove urls' placement in function

--- R/prep_text_noun_phrases.R
+++ R/prep_text_noun_phrases.R
@@ -3,21 +3,30 @@ prep_text_noun_phrases <-function(textdata, groupvar, textvar, node_type=c("grou
 
   #remove URLS
   if (remove_url) {
-    textdata[[textvar]]<-stringr::str_replace_all(textdata[[textvar]], "https?://t\\.co/[A-Za-z\\d]+|https?://[A-Za-z\\d]+|&amp;|&lt;|&gt;|RT|https?", "")
+    textdata<-textdata %>%
+      select_(groupvar,textvar)%>%
+      #remove all URLS
+      mutate_at(textvar, funs(str_replace_all(., "https?://t\\.co/[A-Za-z\\d]+|https?://[A-Za-z\\d]+|&amp;|&lt;|&gt;|RT|https?", ""))) %>%
+      #tidy text
+      mutate_at(textvar, funs(phrasemachine(., maximum_ngram_length = max_ngram_length))) %>%
+      group_by_(groupvar) %>%
+      mutate_at(textvar, funs(paste0(unlist(.),collapse=" "))) %>%
+      unnest_tokens_("word", textvar)  %>%
+      #  #now change names to document and term for consistency
+      ungroup  
   }
-
-  textdata<-textdata %>%
-    select_(groupvar,textvar)%>%
-    #remove all URLS
-    mutate_at(textvar, funs(str_replace_all(., "https?://t\\.co/[A-Za-z\\d]+|https?://[A-Za-z\\d]+|&amp;|&lt;|&gt;|RT|https?", ""))) %>%
-    #tidy text
-    mutate_at(textvar, funs(phrasemachine(., maximum_ngram_length = max_ngram_length))) %>%
-    group_by_(groupvar) %>%
-    mutate_at(textvar, funs(paste0(unlist(.),collapse=" "))) %>%
-    unnest_tokens_("word", textvar)  %>%
-    #  #now change names to document and term for consistency
-    ungroup
-
+  
+  if (remove_url === FALSE) {
+    textdata<-textdata %>%
+      select_(groupvar,textvar)%>%
+      #tidy text
+      mutate_at(textvar, funs(phrasemachine(., maximum_ngram_length = max_ngram_length))) %>%
+      group_by_(groupvar) %>%
+      mutate_at(textvar, funs(paste0(unlist(.),collapse=" "))) %>%
+      unnest_tokens_("word", textvar)  %>%
+      #  #now change names to document and term for consistency
+      ungroup  
+  }
 
   if (node_type=="groups"){
     #count terms by document
