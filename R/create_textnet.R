371fca76c348bc8d4e0c8b02d3781fde93b4f088
Chris Bail <christopher.bail@duke.edu>
2018-02-27 13:19:06 -0500

    Update create_textnet.R
    Speed up by arranging words first

--- R/create_textnet.R
+++ R/create_textnet.R
@@ -13,10 +13,18 @@ create_textnet<-function(tidytextobject, node_type=c("groups","words")){
     for_adjacency<-tidytextobject %>%
       #calculate tfidf
       bind_tf_idf(word, group, count)
-    # I was not able to find a dplyr function that will do this next line
-    # this is too bad because this is the slowest of the functions
-    for_crossprod<-acast(for_adjacency, group~word, sum,
-                         value.var="tf_idf")
+       for_adjacency<-tidytextobject %>%
+      #calculate tfidf
+      bind_tf_idf(word, group, count) %>%
+      #sort on word (MUCH faster
+      arrange(word)
+      for_crossprod<-sparseMatrix(i=match(for_adjacency$group, unique(for_adjacency$group)), 
+                                j=match(for_adjacency$word, unique(for_adjacency$word)), 
+                                x=for_adjacency$tf_idf, 
+                                dimnames=list(unique(for_adjacency$group), 
+                                              unique(for_adjacency$word)))
+      for_crossprod <- for_crossprod[sort(rownames(for_crossprod)),]
+    
     #the line above is not working with the noun phrase function
     #create weighted adjacency matrix
     weighted_adjacency<-tcrossprod(for_crossprod)
@@ -28,8 +36,12 @@ create_textnet<-function(tidytextobject, node_type=c("groups","words")){
      bind_tf_idf(group, word, count)
    # I was not able to find a dplyr function that will do this next line
     # this is too bad because this is the slowest of the functions
-    for_crossprod<-acast(for_adjacency, word~group, sum,
-                         value.var="tf_idf")
+       for_crossprod<-sparseMatrix(i=match(for_adjacency$word, unique(for_adjacency$word)), 
+                                j=match(for_adjacency$group, unique(for_adjacency$group)), 
+                                x=for_adjacency$tf_idf, 
+                                dimnames=list(unique(for_adjacency$word), 
+                                              unique(for_adjacency$group)))
+      for_crossprod <- for_crossprod[sort(rownames(for_crossprod)),]
 
     #the line above is not working with the noun phrase function
     #create weighted adjacency matrix
