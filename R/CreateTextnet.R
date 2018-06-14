446c1505a3857d9e18fc678b29bfda2e6da8d488
fmerhout <friedolin.merhout@duke.edu>
2018-06-14 09:15:54 -0400

    Add PrepTextSent object support
    For PrepTextSent objects, the function now returns an igraph object based on the adjacency matrix of overlapping tfidf-sentiment products.

--- R/CreateTextnet.R
+++ R/CreateTextnet.R
@@ -1,11 +1,12 @@
 #### CREATING NETWORK OBJECTS FROM TEXT DATASETS
 
 # This is the workhorse function of the package, it requires a tidy text
-# dataset, and a) calculates the term frequency inverse document frequency of
-# each word; b) creates a weighted adjacency matrix where the rows and columns
-# are documents and the cells describe the sum of overlapping TFIDF between them;
-# and c) creates an igraph object from this adjacency matrix. The user specifies
-# whether nodes should be documents or words.
+# dataset as created by PrepText or PrepTextSent, and a) calculates the term 
+# frequency inverse document frequency of each word; b) creates a weighted adjacency 
+# matrix where the rows and columns are the groups and the cells describe the sum of 
+# overlapping TFIDF between them; and c) creates an igraph object from this adjacency 
+# matrix. For PrepTextSent objects, b) will be multiplied by the sentiment and the 
+# cells in c) are the overlapping TFIDF-sentiment products.
 
 CreateTextnet <- function(tidytextobject){
   
@@ -13,6 +14,11 @@ CreateTextnet <- function(tidytextobject){
   node_type <- ifelse(names(tidytextobject)[1]=="lemma",
                       "words",
                       "groups")
+  
+  # determine if regular or signed nets
+  nets_type <- ifelse(ncol(tidytextobject)>3,
+                      "signed",
+                      "regular")
   
   # remove grouping allow arranging after adding tfidf
   tidytextobject <- ungroup(tidytextobject)
@@ -36,16 +42,19 @@ CreateTextnet <- function(tidytextobject){
       filter(n() > 1) %>% 
       ungroup()
     
-    # create sparse matrix
+    # create sparse matrix of tfidfs
     # this appears to work and provides a convenient wrapper around sparseMatrix
     # the error appears to be related to some deprecated functions
-    for_crossprod <- cast_sparse(for_adjacency, row = group, col = lemma, value = tf_idf)
-        # for_crossprod <- sparseMatrix(i=match(for_adjacency$group, unique(for_adjacency$group)),
-        #                             j=match(for_adjacency$lemma, unique(for_adjacency$lemma)),
-        #                             x=for_adjacency$tf_idf,
-        #                             dimnames=list(unique(for_adjacency$group),
-        #                                           unique(for_adjacency$lemma)))
+    suppressWarnings(for_crossprod <- cast_sparse(for_adjacency, row = group, col = lemma, value = tf_idf))
     for_crossprod <- for_crossprod[sort(rownames(for_crossprod)),]
+    
+    # create sparse matrix of sentiment scores and multiply with tfidf matrix
+    if(nets_type=="signed"){
+      suppressWarnings(sent_matrix <- cast_sparse(for_adjacency, row = group, col = lemma, value = sentiment))
+      sent_matrix <- sent_matrix[sort(rownames(sent_matrix)),]
+      for_crossprod <- sent_matrix * for_crossprod
+    }
+    
     # the line above is not working with the noun phrase function
     # create weighted adjacency matrix
     weighted_adjacency <- Matrix::tcrossprod(for_crossprod)
@@ -69,17 +78,19 @@ CreateTextnet <- function(tidytextobject){
     # create sparse matrix
     # this appears to work and provides a convenient wrapper around sparseMatrix
     # the error appears to be related to some deprecated functions
-    for_crossprod <- cast_sparse(for_adjacency, row = lemma, col = group, value = tf_idf)
-        # for_crossprod <- sparseMatrix(i=match(for_adjacency$lemma, unique(for_adjacency$lemma)), 
-        #                             j=match(for_adjacency$group, unique(for_adjacency$group)), 
-        #                             x=for_adjacency$tf_idf, 
-        #                             dimnames=list(unique(for_adjacency$lemma), 
-        #                                           unique(for_adjacency$group)))
+    suppressWarnings(for_crossprod <- cast_sparse(for_adjacency, row = lemma, col = group, value = tf_idf))
     for_crossprod <- for_crossprod[sort(rownames(for_crossprod)),]
-    #the line above is not working with the noun phrase function
-    #create weighted adjacency matrix
+    
+    # create sparse matrix of sentiment scores and multiply with tfidf matrix
+    if(nets_type=="signed"){
+      suppressWarnings(sent_matrix <- cast_sparse(for_adjacency, row = group, col = lemma, value = sentiment))
+      sent_matrix <- sent_matrix[sort(rownames(sent_matrix)),]
+      for_crossprod <- sent_matrix * for_crossprod
+    }
+    
+    # create weighted adjacency matrix
     weighted_adjacency <- Matrix::tcrossprod(for_crossprod)
-    #create igraph object
+    # create igraph object
     text_network <- graph.adjacency(weighted_adjacency, mode="undirected", weighted = TRUE, diag = FALSE)
   }
   
