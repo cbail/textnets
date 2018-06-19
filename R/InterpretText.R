c230646e1066251cf2f8e74d504ab6f39467d248
fmerhout <friedolin.merhout@duke.edu>
2018-06-19 15:55:05 -0400

    Make compatible with PrepText update
    - automated renaming of grouping variable
    - lemma instead of word

--- R/InterpretText.R
+++ R/InterpretText.R
@@ -1,20 +1,50 @@
-InterpretText<-function(text_network, tidytext){
+InterpretText <- function(text_network, tidytextobject){
   
   #first create communities (either louvain or fast.greedy)
-  communities<-cluster_louvain(text_network)
+  communities <- cluster_louvain(text_network)
   #optimal modularity kept crashing :(
   #communities<-optimal.community(text_network)
   results<-data.frame(cbind(communities$names, communities$membership),
                       stringsAsFactors = FALSE)
-  names(results)<-c("group","modularity_class")
-  #now merge with tidy text dataframe
+  names(results)<-c("group", "modularity_class")
   
-  tfidf_text<-tidytext %>%
-    bind_tf_idf(group, word, count)
+  #now merge with tidy text dataframe
+  # determine network type
+  node_type <- ifelse(names(tidytextobject)[1]=="lemma",
+                      "words",
+                      "groups")
+  
+  # remove grouping allow arranging after adding tfidf
+  tidytextobject <- ungroup(tidytextobject)
+  
+  # cadd tfidf for node_type groups
+  if(node_type=="groups"){
+    
+    # rename grouping variable for easier referencing
+    names(tidytextobject)[1] <- "group"
+    
+    # add tfidf for adjacency matrix
+    tfidf_text <- tidytextobject %>%
+      # calculate tfidf
+      bind_tf_idf(lemma, group, count)
+  }
+  
+  # add tfidf for node_type words
+  if(node_type=="words"){
+    
+    # rename grouping variable for easier referencing
+    names(tidytextobject)[2] <- "group"
+    
+    # add tfidf for adjacency matrix
+    tfidf_text <- tidytextobject %>%
+      # calculate tfidf
+      bind_tf_idf(group, lemma, count)
+  }
+    
   
   inner_join(tfidf_text, results) %>%
     #summarize words by modularity class
     group_by(modularity_class) %>%
     top_n(n=10, wt=tf_idf) %>%
-    select(modularity_class, word)
+    select(modularity_class, lemma)
 }
